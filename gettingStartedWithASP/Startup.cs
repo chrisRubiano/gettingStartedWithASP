bf8cbddc46e6cec912c20aee390fe23523450394
Cristian Samaniego <chris.rubiano42@gmail.com>
2016-07-10 22:17:37 -0700

    first commit

---
+++ gettingStartedWithASP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gettingStartedWithASP.Startup))]
+namespace gettingStartedWithASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
